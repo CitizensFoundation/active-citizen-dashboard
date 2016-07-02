e9d24125404a8f6b8daac6f3e3926b1ffcaf529b
Róbert Viðar Bjarnason <robert@citizens.is>
2016-07-02 14:16:00 +0000

    Config

--- server_api/models/index.js
+++ server_api/models/index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-      ssl: false
+      ssl: true
     },
     logging: false
   });
