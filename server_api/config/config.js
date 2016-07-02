fb231007f40ad1be4d7142f4e73bb25414daddb1
Róbert Viðar Bjarnason <robert@citizens.is>
2016-07-02 14:07:04 +0000

    Change config

--- server_api/config/config.js
+++ server_api/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     "production": {
         "use_env_variable":"DATABASE_URL",
         "dialect":"postgres",
-        "ssl":true,
+        "ssl":false,
         "dialectOptions":{
             "ssl":{
                 "require":false
