84777f70018e466a1aecdf8726212d4fcd7b1413
Róbert Viðar Bjarnason <robert@citizens.is>
2016-07-02 14:11:58 +0000

    Config

--- server_api/config/config.js
+++ server_api/config/config.js
@@ -2,11 +2,11 @@ module.exports = {
     "production": {
         "use_env_variable":"DATABASE_URL",
         "dialect":"postgres",
-        "ssl":false,
+        "ssl":true,
         "dialectOptions":{
             "ssl":{
-                "require":false
+                "require":true
             }
         }
     }
-}
+};
--- server_api/models/index.js
+++ server_api/models/index.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
   }));
 }
 
-var db        = {};
+var db = {};
 
 // Read from local folder
 fs
