c82543344c300c2346f9be36b13ec5e5bc10f7eb
Robert Bjarnason <Robert Bjarnason>
2017-03-17 01:32:46 +0000

    Various

--- server_api/api_app.js
+++ server_api/api_app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var FORCE_PRODUCTION_MODE = false;
+var FORCE_PRODUCTION_MODE = true;
 
 var debug = require('debug')('active-citizen-dashboard');
 var express = require('express');
