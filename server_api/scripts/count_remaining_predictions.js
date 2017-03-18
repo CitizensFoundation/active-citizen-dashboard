a4f6fc69f23d89359a2a4dbb3de51da8c3efa51e
Robert Bjarnason <Robert Bjarnason>
2017-03-18 21:24:01 +0000

    Added count script

---
+++ server_api/scripts/count_remaining_predictions.js
@@ -0,0 +1,17 @@
+var models = require('../models');
+var async = require('async');
+var fs = require('fs');
+var moment = require('moment');
+
+models.NewsItem.count({
+  where: {
+    rating_value: null,
+    predicted_rating_value: null,
+    created_at: {
+      $gt:  moment().add(-1, 'weeks').toISOString()
+    }
+  }
+}).then(function (count) {
+  console.log("You have "+count+" remaining");
+  process.exit();
+});
